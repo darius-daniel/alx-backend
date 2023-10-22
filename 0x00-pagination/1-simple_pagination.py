af4b41094750b68b653ac268c364c91acb6499da
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-23 00:01:24 +0100

    Pushing scripts for task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+""" 0. Simple Helper Function. """
+import math
+import csv
+from typing import Tuple, List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple containing a start index and an end index
+    :param page:
+    :param page_size: number of items on the page
+    :return: a tuple
+    """
+    start = (page - 1) * page_size
+
+    return start, start + page_size
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert type(page) is int
+        assert type(page_size) is int
+        assert page > 0
+        assert page_size > 0
+
+        output = []
+
+        with open('Popular_Baby_Names.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for index, row in enumerate(reader):
+                start, end = index_range(page, page_size)
+                if start <= index <= end:
+                    output.append(row)
+
+        return output
