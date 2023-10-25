000f7335fdb07f056e2a8101a861210cf2b84f03
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-25 03:05:38 +0100

    Made conditional less verbose

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -16,7 +16,7 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """ Adds an item in the cache
         """
-        if not (key is None or item is None):
+        if key and item:
             self.key_cache.append(key)
             self.cache_data[key] = item
 
