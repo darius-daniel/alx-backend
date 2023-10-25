53a7761ea88c234fa21238ea6a0435312d6c9e09
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-25 01:33:42 +0100

    Adding scripts for task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+""" 4. MRU Caching. """
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+def get_mru(cache):
+    """ Returns the least recently used key
+    """
+    least_recent = max(cache.values())
+    print(cache)
+    for k, v in cache.items():
+        if v == least_recent:
+            return k
+
+
+class MRUCache(BaseCaching):
+    """ LRUCache
+    - Inherits from BaseCaching
+    """
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+        self.key_cache = OrderedDict()
+
+    def put(self, key, item):
+        """ Adds an item in the cache
+        """
+        if not (key is None or item is None):
+            if not self.key_cache:
+                self.key_cache[key] = 0
+            else:
+                self.key_cache[key] = max(list(self.key_cache.values())) + 1
+
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded_key = get_mru(self.key_cache)
+
+            print('DISCARD: {}'.format(discarded_key))
+            del self.cache_data[discarded_key]
+            del self.key_cache[discarded_key]
+
+    def get(self, key):
+        """ Gets an item by key
+        """
+        if not (key is None or key not in self.cache_data.keys()):
+            return self.cache_data[key]
