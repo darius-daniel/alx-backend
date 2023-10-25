25a9bb4abcd3cc8987cf85503c02c7140dcf1ff9
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-25 03:03:17 +0100

    Reverted to earliest changes

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -16,7 +16,7 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """ Adds an item in the cache
         """
-        if key is not None and item is not None:
+        if key or item:
             self.key_cache.append(key)
             self.cache_data[key] = item
 
@@ -27,7 +27,5 @@ class LIFOCache(BaseCaching):
     def get(self, key):
         """ Gets an item by key
         """
-        if key and key not in self.cache_data.keys():
+        if not (key is None or key not in self.cache_data.keys()):
             return self.cache_data[key]
-        else:
-            return None
